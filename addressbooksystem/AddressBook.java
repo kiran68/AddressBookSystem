49a49bd1c756b0974df3a5e85fb2722f6894d44e
kiran68 <kiranurshil88@gmail.com>
2023-03-30 17:57:03 +0530

    Create Contacts In Address Book

---
+++ addressbooksystem/AddressBook.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBook {
+
+
+}
---
+++ addressbooksystem/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Address Book System");
+	
+	}
+
+}
