cf01eca4a7ed7395102a90d2a0ec3cc9c3a50a0c
kiran68 <kiranurshil88@gmail.com>
2023-04-04 10:44:50 +0530

    Add: delete Contact In Address Book System

--- addressbooksystem/AddressBookMain.java
+++ addressbooksystem/AddressBookMain.java
@@ -14,6 +14,7 @@ public class AddressBookMain {
 			System.out.println("Enter 1 to add contacts");
 			System.out.println("Enter 2 to display all contacts");
 			System.out.println("Enter 3 to edit a contact");
+			System.out.println("Enter 4 to delete a contact");
 			System.out.println("Enter 0 to exit");
 
 			int choice = scanner.nextInt();
@@ -36,6 +37,17 @@ public class AddressBookMain {
 				String lastName = scanner.nextLine();
 				addressBook.editPerson(firstName, lastName);
 				break;
+
+			case 4:
+				System.out.println("Enter the first name of the contact to delete :");
+				String firstName1 = scanner.nextLine();
+				System.out.println("Enter the last name of the contact delete:");
+				String lastName1 = scanner.nextLine();
+				addressBook.deleteContact(firstName1, lastName1);
+				break;
+			default:
+				System.out.println("Contact details not found");
+
 			}
 		}
 
